3252057442e7c73a0bdc1d264aa6b99fb33baa61
lavinia1303 <lavinia.mariano@edu.senai.br>
2026-05-27 09:00:37 -0300

    cadastro pet

---
+++ teste2.py
@@ -0,0 +1,37 @@
+banco_de_dados = []
+
+def cadastrar_pet(nome, idade, raca, situacao ):
+    novo_pet = {
+        "nome": nome,
+        "idade": idade,
+        "raca": raca,
+        "situacao": situacao,
+    }
+    banco_de_dados.append(novo_pet)
+    print(f"\n {nome} cadastrado com sucesso!\n")
+
+# Loop principal de interação
+while True:
+    print("--- MENU DE CADASTRO ---")
+    print("1. Cadastrar novo pet")
+    print("2. Sair")
+    
+    opcao = input("Escolha uma opção (1/2): ")
+
+    if opcao == "1":
+        # Coleta os dados do usuário pelo teclado
+        nome_input = input("Digite o nome: ")
+        idade_input = input("Digite a idade: ")
+        raca_input = input ("Digite a raça de seu animal: ")
+        situacao_input= input("Digite qual o estado que se animal está: ")
+        
+        # Envia os dados para a função
+        cadastrar_pet(nome_input, idade_input, raca_input, situacao_input)
+        
+    elif opcao == "2":
+        print("Saindo do sistema... Até logo!")
+        exit()
+    else:
+        print("\n Opção inválida! Tente novamente.\n")
+
+        
